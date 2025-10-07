5972ca34afff28c7f3c6d7c4240bc7dd1d157720
Afdhal <leptopafdhal@gmail.com>
2025-10-07 12:57:24 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia minima nulla animi fugiat similique pariatur quod illo explicabo laudantium magnam voluptatum, alias reiciendis eligendi quasi dolorem corporis eum amet eius.</p>
+</body>
+</html>
